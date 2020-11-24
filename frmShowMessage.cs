8245a3d8d7a5d806299d57a3e23676968ca0b4f5
mtdk <ruixiangxie@outlook.com>
2020-11-24 18:12:08 +0800

    202011241812

---
+++ frmShowMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace MessageBoxDemo
+{
+    public partial class frmShowMessage : Form
+    {
+        public frmShowMessage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Rectangle rect = this.ClientRectangle;
+            LinearGradientBrush brush = new LinearGradientBrush(rect, Color.SkyBlue, Color.AliceBlue, 60);
+            e.Graphics.FillRectangle(brush, rect);
+            base.OnPaint(e);
+        }
+
+
+    }
+}
